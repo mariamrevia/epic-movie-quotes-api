1458861bbb7535af47b7d0d644e67823258bb624
mariamrevia <mariam.revia@redberry.ge>
2023-06-20 22:25:55 +0400

    refactor:user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,11 @@ return new class () extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('google_id')->unique()->nullable();
             $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->string('google_id')->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
