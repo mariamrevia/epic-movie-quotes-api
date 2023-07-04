77f1b37c074e6bc399f9231720adfb09c33bedb7
mariamrevia <mariam.revia@redberry.ge>
2023-07-04 22:46:52 +0400

    refacor:move foreign ids

--- database/migrations/2023_06_21_072331_create_genre_movie_table.php
+++ database/migrations/2023_06_21_072331_create_genre_movie_table.php
@@ -13,9 +13,9 @@ return new class () extends Migration {
     {
         Schema::create('genre_movie', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->foreignId('movie_id')->constrained()->cascadeOnDelete();
             $table->foreignId('genre_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
 
         });
     }
