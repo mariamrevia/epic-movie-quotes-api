eeae5bbb053e27a4121199eb18e8dde17a2703d7
mariamrevia <mariam.revia@redberry.ge>
2023-07-17 08:39:11 -0700

    refactor:routes

--- routes/api.php
+++ routes/api.php
@@ -47,40 +47,37 @@ Route::group(['middleware' => ['web']], function () {
 
 });
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(MovieController::class)->group(function () {
+        Route::get('/movies', 'show')->name('movies.show_all');
+        Route::post('/movies', 'store')->name('movies.store');
+        Route::patch('/movies/{movieId}', 'update')->name('movies.update');
+        Route::get('/movies/search', 'show')->name('movies.show');
+        Route::delete('/movies/{movie}', 'destroy')->name('movies.destory');
+    });
 
-Route::controller(MovieController::class)->group(function () {
-    Route::get('/movies', 'show')->name('movies.show_all');
-    Route::post('/movies', 'store')->name('movies.store');
-    Route::patch('/movies/{movieId}', 'update')->name('movies.update');
-    Route::get('/movies/search', 'show')->name('movies.show');
-    Route::delete('/movies/{movie}', 'destroy')->name('movies.destory');
+    Route::controller(QuoteContoller::class)->group(function () {
+        Route::get('/quotes', 'show')->name('quotes.show_all');
+        Route::post('/quotes', 'store')->name('quotes.store');
+        Route::patch('quotes{quote}', 'update')->name('quotes.update');
+        Route::get('/quotes/search', 'show')->name('quotes.show');
+        Route::delete('/quotes/{quote}', 'destroy')->name('quotes.destroy');
+    });
+
+    Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::controller(LikeController::class)->group(function () {
+        Route::post('/likes', 'store')->name('likes.store');
+        Route::delete('/likes/{quoteId}', 'destroy')->name('likes.destroy');
+
+    });
+    Route::controller(NotificationController::class)->group(function () {
+        Route::post('/notifications/{movie}/like', 'like')->name('notification.like');
+        Route::get('/notifications/{user}', 'show')->name('notification.show');
+        Route::post('/notifications/{movie}/comment', 'comment')->name('notification.comment');
+        Route::patch('/notifications/markread/{user}', 'markread')->name('notification.markread');
+
+    });
 });
-
-Route::controller(QuoteContoller::class)->group(function () {
-    Route::get('/quotes', 'show')->name('quotes.show_all');
-    Route::post('/quotes', 'store')->name('quotes.store');
-    Route::patch('quotes{quote}', 'update')->name('quotes.update');
-    Route::get('/quotes/search', 'show')->name('quotes.show');
-    Route::delete('/quotes/{quote}', 'destroy')->name('quotes.destroy');
-});
-
-Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::controller(LikeController::class)->group(function () {
-    Route::post('/likes', 'store')->name('likes.store');
-    Route::delete('/likes/{quoteId}', 'destroy')->name('likes.destroy');
-
-});
-
-
-Route::controller(NotificationController::class)->group(function () {
-    Route::post('/notifications/{movie}/like', 'like')->name('notification.like');
-    Route::get('/notifications/{user}', 'show')->name('notification.show');
-    Route::post('/notifications/{movie}/comment', 'comment')->name('notification.comment');
-    Route::patch('/notifications/markread/{user}', 'markread')->name('notification.markread');
-
-});
-
-
 Route::controller(UserController::class)->group(function () {
     Route::get('/profile/email/verify/{id}/{hash}', 'verify')->middleware('signed')->name('email.verification_verify');
     Route::patch('/user/{user}', 'update')->name('user.update');
